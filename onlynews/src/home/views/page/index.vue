154dcc93cf0225964689d528040e00a48f7327bd
yang_da <1341521959@qq.com>
2024-05-31 03:33:00 +0800

    add wx app test

--- onlynews/src/home/views/page/index.vue
+++ onlynews/src/home/views/page/index.vue
@@ -1,12 +1,33 @@
+<!--
+ * @Author: yangda
+ * @Date: 2024-05-31 03:22:17
+ * @LastEditTime: 2024-05-31 03:28:52
+ * @Descripttion: 
+-->
 <template>
-  <div>
-    welcome to new and routermaker and all lest  not module read
+  <div @click="sendMessage">
+    {{ title }}
   </div>
 </template>
 
 <script>
 export default  {
-
+  data() {
+    return {
+      title: 'welcome to web '
+    }
+  },
+  mounted(){
+    window.addEventListener('message', this.messageHandler)
+  },
+  methods: {
+    messageHandler(e){
+      this.title = e.data
+    },
+    sendMessage(){
+      window.parent.postMessage("data from web", '*')
+    }
+  }
 }
 </script>
 
